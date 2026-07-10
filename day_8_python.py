c52822974c3d85f43ded61a49dce9d8128ffec19
jamil19-humii <humiijamila77@gmail.com>
2026-07-10 23:35:26 +0300

    Day 8 of python

--- day_8_python.py
+++ day_8_python.py
@@ -34,3 +34,19 @@ print("Data type of skills:", type(student['skills']))
 student['skills'].append('JavaScript')
 student['skills'].append('HTML')
 print("Updated Skills:", student['skills'])
+print("---------------------------------------------------------------- ")
+#get dict keys as a list
+keys=student.keys()
+print(keys)
+print("---------------------------------------------------------------- ")
+#get dict values as a list
+values=student.values()
+print(values)
+print("---------------------------------------------------------------- ")
+#change the dictionary to a list of tuples using items() method
+student_items=list(student.items())
+print("Student Dictionary as list of tuples:", student_items)
+print("---------------------------------------------------------------- ")
+#delete one of the items in the dictionary
+del student['marital_status']
+print("Dictionary after deleting 'marital_status':", student)
